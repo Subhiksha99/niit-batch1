6bf13b8cca964ae7f17009cf3d5a4ed7f1b284f6
sudheerr <testusr680@gmail.com>
2023-08-04 17:41:25 +0530

    Added OOP's concepts code

--- Java Basics/src/day3/EncapsulationDemo.java
+++ Java Basics/src/day3/EncapsulationDemo.java
@@ -24,5 +24,17 @@ class EmployeeDetails {
 public class EncapsulationDemo {
     public static void main(String[] args) {
         EmployeeDetails ed1 = new EmployeeDetails();
+        // read
+        //sout(ed1.name);
+        //sout(ed1.salary);
+
+        //ed1.name="Kumar";
+        //ed1.salary=20000.00;
+
+        System.out.println(ed1.getEmpId());
+        System.out.println(ed1.getName());
+
+        ed1.setName("Shiv Kumar");
+        System.out.println(ed1.getName());
     }
 }

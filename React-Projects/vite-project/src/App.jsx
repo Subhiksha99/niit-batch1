a41d810ec0e716953ac7c381d22e12606c7f7685
rskumar780@gmail.com <rskumar780@gmail.com>
2023-09-05 15:57:22 +0530

    Added Products & counter component

--- React-Projects/vite-project/src/App.jsx
+++ React-Projects/vite-project/src/App.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import { Nav } from './components/nav'
 import Login from './components/login';
+import Counter from './components/counter';
+import CounterFunc from './components/counterFunc';
+import Products from './components/products';
 
 function App() {
 
 
+
   return (<React.Fragment>
     <Nav />
+    <Products />
     <Login />
+    <Counter />
+    <CounterFunc />
   </React.Fragment >)
 }
 
